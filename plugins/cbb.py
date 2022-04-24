5bf7317ff012fd375eec13c5e9e7639a9f09fd07
shiroxynn <94505216+ReyyNada@users.noreply.github.com>
2022-04-24 20:15:37 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/ReyyNada/Reyy-XFsub-Bot2'>Click here</a>\n○ Channel : @yagaesyakkk\n○ Owner : @shiroxynn</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/ReyyNada/Reyy-XFsub-Bot2'>Click here</a>\n○ Channel : @stayhall4l\n○ Owner : @shiroxynn</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
